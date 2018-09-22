90035450c455b1e63221dfa1aebe4a672568e06f
Miguel H <mher.harutyunyan@innocv.com>
2018-09-22 19:46:22 +0200

    Commit Guay 2

--- src/main/java/com/innocv/votium/controller/TeamController.java
+++ src/main/java/com/innocv/votium/controller/TeamController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TeamController {
 
