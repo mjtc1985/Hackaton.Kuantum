bcefcc18d0a4f2892f384dcae81bf4eb51976baa
manueljesus.talavera <manueljesus.talavera@innocv.com>
2018-09-22 18:17:45 +0200

    port

--- src/main/java/com/innocv/votium/WebSecurityConfig.java
+++ src/main/java/com/innocv/votium/WebSecurityConfig.java
@@ -1,13 +1,13 @@
-package com.innocv.votium;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel().anyRequest().requiresSecure();
-    }
-}
+//package com.innocv.votium;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.requiresChannel().anyRequest().requiresSecure();
+//    }
+//}
