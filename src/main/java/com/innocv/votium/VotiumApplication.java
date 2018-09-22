25a8bcb4c83b82678b38504e5b4917e7fe59426c
manueljesus.talavera <manueljesus.talavera@innocv.com>
2018-09-22 18:03:21 +0200

    add configuration

--- src/main/java/com/innocv/votium/VotiumApplication.java
+++ src/main/java/com/innocv/votium/VotiumApplication.java
@@ -2,10 +2,13 @@ package com.innocv.votium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class VotiumApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
