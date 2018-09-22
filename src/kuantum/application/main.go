a4d3c4a28ee2cf0fa96749b398e3a7285c312969
manueljesus.talavera <manueljesus.talavera@innocv.com>
2018-09-22 13:35:49 +0200

    add mock main

--- src/kuantum/application/main.go
+++ src/kuantum/application/main.go
@@ -1,1 +1,29 @@
 package application
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	var people []Person
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe"})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe"})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+	json.NewEncoder(w).Encode(people)
+}
