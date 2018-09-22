face56d4ef2e71cf3a3012aaa7100951604e36c8
Cristiam Sosa <cristiam.sosa@innocv.com>
2018-09-22 20:13:23 +0200

    Add users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,32 @@
 insert into team(id, name) values (1, 'Qc0nn');
 insert into team(id, name) values (2, 'Kuantium');
 insert into team(id, name) values (3, 'ElVivi');
+
+insert into member(id, userId, fullName) values (1, "1", "Alfonso Conti Morera")
+insert into member(id, userId, fullName) values (2, "2", "Carlos Enrique Alonso")
+insert into member(id, userId, fullName) values (3, "3", "Daniel Manuel Ayuso Gómez-Pimpollo")
+insert into member(id, userId, fullName) values (4, "4", "Eugenio Barquin")
+insert into member(id, userId, fullName) values (5, "5", "Pablo Agustín Barca Martínez")
+insert into member(id, userId, fullName) values (6, "6", "Sergio Juntas")
+insert into member(id, userId, fullName) values (7, "7", "Sergio López Jurado")
+insert into member(id, userId, fullName) values (8, "8", "Sofía Rodríguez Olmedo")
+insert into member(id, userId, fullName) values (9, "9", "Vanesa Jerez Molinero")
+
+insert into member(id, userId, fullName) values (10, "10", "Adrián Ruiz ")
+insert into member(id, userId, fullName) values (11, "11", "Cristiam Sosa Urviola")
+insert into member(id, userId, fullName) values (12, "12", "Jacobo González Pedraza")
+insert into member(id, userId, fullName) values (13, "13", "Jessica Castro Collazo")
+insert into member(id, userId, fullName) values (14, "14", "Juan Diego Garcia Corbacho")
+insert into member(id, userId, fullName) values (15, "15", "Manuel Jesús Talavera Calzado")
+insert into member(id, userId, fullName) values (16, "16", "Mher Harutyunyan")
+insert into member(id, userId, fullName) values (17, "17", "Nuria Mateos Sánchez")
+insert into member(id, userId, fullName) values (18, "18", "Salvador Cano Iglesias")
+
+insert into member(id, userId, fullName) values (19, "19", "Daniel Rivero Losa")
+insert into member(id, userId, fullName) values (20, "20", "David Sierra Estornes")
+insert into member(id, userId, fullName) values (21, "21", "Isabel Clara Sanchez Mendo")
+insert into member(id, userId, fullName) values (22, "22", "Javier Giles Moreno")
+insert into member(id, userId, fullName) values (23, "23", "José Vicente Bañuls")
+insert into member(id, userId, fullName) values (24, "24", "Luis Mochales López-Serrano")
+insert into member(id, userId, fullName) values (25, "25", "Mario Muñoz Castellanos")
+insert into member(id, userId, fullName) values (26, "26", "Víctor Jiménez García")
