e0e37f8155929a52596ee50d39255dfbd73e7267
manueljesus.talavera <manueljesus.talavera@innocv.com>
2018-09-22 18:35:20 +0200

    fix login

--- src/main/java/com/innocv/votium/controller/LoginController.java
+++ src/main/java/com/innocv/votium/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.innocv.votium.controller;
 
 import com.innocv.votium.dto.LoginDto;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @PostMapping("login")
-    private LoginDto login(LoginDto loginDto){
+    private LoginDto login(@RequestBody LoginDto loginDto){
         return loginDto;
     }
 
